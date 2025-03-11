b145d6888e7a9c3c74ac1676260e2b9646e0e233
avi8230 <avi8230@gmail.com>
2025-03-11 15:58:03 +0200

    Remove localhost:3000 from CORS origins in server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ require("./05-config/db"); // Connect to database
 
 const app = express();
 app.use(cors({
-    origin: ["http://localhost:5173", "http://localhost:3000", "https://www.google.com", "https://www.facebook.com"],
+    origin: ["http://localhost:5173", "https://www.google.com", "https://www.facebook.com"],
     credentials: true // Enable credentials - cookies
 }));
 app.use(express.json());
